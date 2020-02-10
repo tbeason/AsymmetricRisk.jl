f077a6a6eb634fa2325eccc177be6c0da977bfbc
Tyler Beason <tbeas12@gmail.com>
2020-02-10 12:57:22 -0700

    VaR and ES partially in

--- src/AsymmetricRisk.jl
+++ src/AsymmetricRisk.jl
@@ -5,10 +5,12 @@ using StatsBase
 using KernelDensity
 using HCubature
 using Distributions
+using EmpiricalCDFs
+import QuadGK: quadgk
 # using GLM
 # using NamedTupleTools
 
-export semivariance, lowerpartialmom, upperpartialmom
+export semivariance, lowerpartialmom, upperpartialmom, valueatrisk, expectedshortfall
 
 export quantiledep, exceedancecor, downsidebeta, upsidebeta, coskewness, cokurtosis
 
--- src/univariate.jl
+++ src/univariate.jl
@@ -55,3 +55,56 @@ end
 
 
 
+
+"""
+    valueatrisk(D::ContinuousUnivariateDistribution,alpha)
+
+Returns the VaR (value at risk) at the `alpha`-level for the distribution `D`.
+"""
+valueatrisk(D::Distributions.ContinuousUnivariateDistribution,alpha::Float64) = quantile(D,1-alpha)
+
+"""
+    valueatrisk(X::AbstractVector,alpha)
+
+Returns the nonparametric estimate of VaR (value at risk) at the `alpha`-level for `X`.
+"""
+function valueatrisk(X::AbstractVector,alpha::Float64)
+    ecdf = EmpiricalCDF()
+    append!(ecdf,X)
+    sort!(ecdf)
+    icdf = finv(ecdf)
+    return icdf(1-alpha)
+end
+
+
+
+"""
+    expectedshortfall(D::ContinuousUnivariateDistribution,alpha)
+
+Returns the Expected Shortfall at the `alpha`-level for the distribution `D`.
+Assumes `D` is the distribution of payoffs `X`, not losses (which would be `-X`).
+"""
+function expectedshortfall(D::ContinuousUnivariateDistribution,alpha::Float64)
+    return first(quadgk( x -> -(1/alpha)*valueatrisk(D,x),0.0,alpha))
+end
+
+
+"""
+    expectedshortfall(X::AbstractVector,alpha)
+
+Returns the nonparametric estimate of Expected Shortfall at the `alpha`-level for `X`.
+Assumes `X` represents payoffs, not losses (which would be `-X`).
+"""
+function expectedshortfall(X::AbstractVector,alpha::Float64)
+    ecdf = EmpiricalCDF()
+    append!(ecdf,X)
+    sort!(ecdf)
+    icdf = finv(ecdf)
+    return first(quadgk( x -> -(1/alpha)*icdf(1-x),0.0,alpha))
+end
+
+
+# Bertsimas, Lauprete, Samarov (2004 JEDC) good source for ES/VaR/LPM
+
+
+
